6ce13065a9a2e643a91852b2ef1896c4d42614a5
marti nogueira <martinianonogueira@gmail.com>
2025-05-29 13:00:14 -0300

    Fix Auth

--- backend/Routes/AuthRoutes.js
+++ backend/Routes/AuthRoutes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const { registrarUsuario } = require('../controllers/authController');
+const { registrarUsuario } = require('../Controllers/authController');
 
 router.post('/registrarUsuario', registrarUsuario);
 
