ab0a2fef6d96b14d92c4fce4fc40c36c5cc2ec7d
vmartinbotter <vmartinbotter@mail.austral.edu.ar>
2025-05-29 11:12:29 -0300

    merge

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Welcome from './Components/Welcome';
+import Login from './Components/Login';
+import Register from './Components/Register';
+import Home from './Components/Home';
+import AuthorizeEntry from './Components/AuthorizeEntry';
+import './index.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Welcome />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/home" element={<Home />} />
+          <Route path="/authorize-entry" element={<AuthorizeEntry />} />
+        </Routes>
+      </Router>
   );
 }
 
-export default App;
+export default App;
