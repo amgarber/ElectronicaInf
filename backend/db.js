2142618627320856521e0bda6726c7c9c754f9be
vmartinbotter <vmartinbotter@mail.austral.edu.ar>
2025-05-29 14:26:58 -0300

    env y db.js

--- backend/db.js
+++ backend/db.js
@@ -3,7 +3,7 @@ const { Pool } = require('pg');
 require('dotenv').config();
 
 const pool = new Pool({
-    connectionString: process.env.DATABASE_URL, // o configuraciones separadas
+    connectionString: process.env.DATABASE_URL,
     ssl: process.env.NODE_ENV === 'production' ? { rejectUnauthorized: false } : false
 });
 
