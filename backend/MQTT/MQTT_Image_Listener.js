73efae9a84ee626dae53556b0b168f4e06feee49
azulg <agarber@mail.austral.edu.ar>
2025-06-15 13:59:28 -0300

    Listener

--- backend/MQTT/MQTT_Image_Listener.js
+++ backend/MQTT/MQTT_Image_Listener.js
@@ -24,6 +24,8 @@ const dbConfig = {
 AWS.config.update({ region: 'us-east-1' });
 const rekognition = new AWS.Rekognition();
 const s3 = new AWS.S3();
+const publicUrl = `https://${BUCKET_NAME}.s3.amazonaws.com/${s3Key}`;
+
 
 // === VARIABLES DE ESTADO ===
 let ultimaPatenteDetectada = null;
@@ -206,7 +208,7 @@ client.on('message', async (topic, message) => {
                 await db.query('INSERT INTO vehiculos (patente) VALUES ($1) ON CONFLICT (patente) DO NOTHING', [patente]);
                 await db.query(
                     'INSERT INTO solicitudes_manuales (patente, fecha_hora, imagen_url, estado) VALUES ($1, NOW(), $2, $3)',
-                    [patente, key, 'pendiente']
+                    [patente, publicUrl, 'pendiente']
                 );
                 await db.end();
                 console.log(`📝 Solicitud manual registrada para ${patente}`);
